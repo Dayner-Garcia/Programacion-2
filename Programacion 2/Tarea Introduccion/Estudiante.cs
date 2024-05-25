ce96548dd45019c439618c1963b2408d2c041242
Dayner <164125521+Dayner-Garcia@users.noreply.github.com>
2024-05-25 09:43:46 -0400

    Agregar archivos de proyecto.

---
+++ Programacion 2/Tarea Introduccion/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿namespace Capitulo_1_Introduccion.Tarea_Introduccion
+{
+    internal class Estudiante : MiembroComunidad
+    {
+        private int estudianteID {  get; set; }
+        private string matricula {  get; set; }
+        private string carrera { get; set; }
+        private Boolean activo { get; set; }
+        public Estudiante(string nombre, int edad, string cedula, string correo, int estudianteID, String matricula, string carrera) : base(nombre, edad, cedula, correo)
+        {
+            this.estudianteID = estudianteID;
+            this.matricula = matricula;
+            this.carrera = carrera;
+            this.activo = true;
+        }
+    }
+}
